9fec9215bbbfe542d8b85aa66c03f9379296d7ba
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-02-27 08:58:41 -0300

    1ª versão

---
+++ src/main/kotlin/funcoes/Exerc1.kt
@@ -0,0 +1,14 @@
+package funcoes
+
+//funcoes/VarArgs.kt
+
+fun main(args: Array<String>){
+
+    for (n in ordenar(38, 3, 456, 8, 51, 1, 88, 73)){
+        print("$n ")
+    }
+}
+
+fun ordenar(vararg numeros: Int): IntArray{
+    return numeros.sortedArray()
+}
